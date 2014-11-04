4cd6bbc19f812f0a0879f2ffe42b2b3dfc1c278c
Guillaume <Guillaume@Guillaume>
2014-11-04 12:11:26 +0100

    #4 classes CallDetector et FlipService créées

---
+++ src/com/ihm/smartdring/CallDetector.java
@@ -0,0 +1,19 @@
+package com.ihm.smartdring;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CallDetector extends BroadcastReceiver {
+
+	/**
+	 * Actions to do when receiving a call
+	 * Reacts to CALL_STATE_RINGING
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/ihm/smartdring/FlipService.java
@@ -0,0 +1,30 @@
+package com.ihm.smartdring;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.IBinder;
+
+public class FlipService extends Service implements SensorEventListener {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
