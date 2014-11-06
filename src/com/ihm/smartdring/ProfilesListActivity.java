a3d8cf1a6ca4744c635182cbbe38954e19025cda
loicviguier <loicviguier@gmail.com>
2014-11-06 23:08:23 +0100

    #10 Update icône application et interface
    Ajout des listes avec radio button dans ProfilesListActivity.java.
    Refonte de l’icone.

--- src/com/ihm/smartdring/ProfilesListActivity.java
+++ src/com/ihm/smartdring/ProfilesListActivity.java
@@ -1,17 +1,24 @@
 package com.ihm.smartdring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ProfilesListActivity extends Activity {
-	//private Button addProfile = null;
+	private ListView profilesListView = null;
 	private Intent profileSettingActivity = null;
 	
 	private void initialize() {
-		//this.addProfile = (Button)findViewById(R.id.menu_add_profile);
+		this.profilesListView = (ListView) findViewById(R.id.listViewProfiles);
 		this.profileSettingActivity = new Intent(this, ProfileSetupActivity.class);
 	}
 
@@ -21,6 +28,24 @@ public class ProfilesListActivity extends Activity {
 		setContentView(R.layout.profiles_list_activity);
 		
 		this.initialize();
+		
+		List<String> exemple = new ArrayList<String>();
+		exemple.add("Home");
+		exemple.add("Work");
+		exemple.add("Sport");
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, exemple);
+		this.profilesListView.setAdapter(adapter);
+		
+		profilesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				// Implement action on item click here
+			}
+			
+		});
 		
 	}
 
@@ -43,4 +68,5 @@ public class ProfilesListActivity extends Activity {
 		}
 		
 	}
+	
 }
