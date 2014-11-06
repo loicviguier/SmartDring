222fd2cf746c5558a55747001ff0eeb94edd1ab9
loicviguier <loicviguier@gmail.com>
2014-11-06 16:55:34 +0100

    #10 Implémentation Interface
    Implémentation menu pour profile_setup_activity.xml
    Implémentation menu pour profiles_list_activity.xml
    Ajouts icones

--- src/com/ihm/smartdring/ProfileSetupActivity.java
+++ src/com/ihm/smartdring/ProfileSetupActivity.java
@@ -33,11 +33,14 @@ public class ProfileSetupActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        
+    	switch(item.getItemId()) {
+			case R.id.menu_discard_profile:
+				finish();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+    	}
     }
     
     /**
--- src/com/ihm/smartdring/ProfilesListActivity.java
+++ src/com/ihm/smartdring/ProfilesListActivity.java
@@ -1,16 +1,27 @@
 package com.ihm.smartdring;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class ProfilesListActivity extends Activity {
+	//private Button addProfile = null;
+	private Intent profileSettingActivity = null;
+	
+	private void initialize() {
+		//this.addProfile = (Button)findViewById(R.id.menu_add_profile);
+		this.profileSettingActivity = new Intent(this, ProfileSetupActivity.class);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profiles_list_activity);
+		
+		this.initialize();
+		
 	}
 
 	@Override
@@ -22,13 +33,14 @@ public class ProfilesListActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		
+		switch(item.getItemId()) {
+			case R.id.menu_add_profile:
+				startActivity(this.profileSettingActivity);
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+		
 	}
 }
